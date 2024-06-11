f0b5a0443883751a0304ceb4c80c578268c87478
pierrezoleta <pierrezoleta@gmail.com>
2024-06-11 09:55:42 +0800

    i love android studio ui

---
+++ app/src/main/java/com/example/snap/LegitSplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.snap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LegitSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_legit_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LegitSplashActivity.this, SplashScreenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
